75b6900697d21ba83c13e01eb2c6ad2f022ed5d8
github username <joshmrallen@gmail.com>
2020-08-26 10:32:17 -0400

    added key event loggin and move button listeners

--- index.js
+++ index.js
@@ -2,5 +2,43 @@ document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
 
+  const ul = document.querySelector('#moves-container');
+
   // ADD CODE HERE!
-})
+  document.addEventListener("keydown", function(e){
+    let li = document.createElement('li');
+
+    switch(e.key) {
+      case "ArrowDown":
+        //do something
+        li.innerText = "down"
+        break;
+      case "ArrowUp":
+        //do something
+        li.innerText = "up"
+        break;
+      case "ArrowLeft":
+        //do something
+        li.innerText = "left"
+        break;
+      case "ArrowRight":
+        //do something
+        li.innerText = "right"
+        break;
+    }
+
+    ul.append(li)
+
+  });
+
+  const button = document.querySelector('#move-button');
+
+  button.addEventListener("click", function(){
+    let moveInstruction = ul.firstChild;
+    move(moveInstruction.innerText);
+    moveInstruction.remove();
+  });
+
+
+
+});
